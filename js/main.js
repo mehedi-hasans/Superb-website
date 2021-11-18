5324f7ee798bbd6f9bf78cb8cc3cea09e3784dc1
mehedi115 <shihab3676@gmail.com>
2021-11-18 02:37:41 -0500

    html syntex adden

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('.menu').slicknav();
+
+$('.slider-full').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.all_testimonial').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1400,
+    dots: true,
+   
+  });
+ 
